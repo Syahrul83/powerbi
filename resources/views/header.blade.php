1858f1776df1b79f7d8b51a194a0e835c7bfd8c7
syahrul anwar <syahrulanwar83@gmail.com>
2022-10-19 21:31:19 +0800

    faVICPN DIS

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -20,8 +20,8 @@
 		<meta name="author" content="okler.net">
 
 		<!-- Favicon -->
-		<link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon" />
-		<link rel="apple-touch-icon" href="/img/apple-touch-icon.png">
+		<link rel="shortcut icon" href="/img/_favicon.ico" type="image/x-icon" />
+		<link rel="apple-touch-icon" href="/img/_apple-touch-icon.png">
 
 		<!-- Mobile Metas -->
 		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no">
